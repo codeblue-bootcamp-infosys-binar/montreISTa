622c9b2fa308f2099c9985116212101ccbb8f938
unknown <salahuddinade@gmail.com>
2022-08-01 14:57:38 +0700

    initial commit

---
+++ src/main/java/com/codeblue/montreISTA/MontreIstaApplication.java
@@ -0,0 +1,13 @@
+package com.codeblue.montreISTA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontreIstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontreIstaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeblue/montreISTA/MontreIstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeblue.montreISTA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontreIstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
