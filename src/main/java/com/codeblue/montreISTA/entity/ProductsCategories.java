7fcec37a1317b59cf7d9a6ab71aefbe06e63ebac
Bayu Aji <bayuapamungkas@yahoo.com>
2022-08-01 21:13:27 +0700

    + product and product category entities

---
+++ src/main/java/com/codeblue/montreISTA/entity/ProductsCategories.java
@@ -0,0 +1,29 @@
+package com.codeblue.montreISTA.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "product_category")
+public class ProductsCategories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long productCategoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Products products;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Categories category;
+}
