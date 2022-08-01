475a63ca124f66c501506751f88d2144a3533c7b
Rifhub <105769135+Rifhub@users.noreply.github.com>
2022-08-01 23:07:14 +0700

    f

--- src/main/java/com/codeblue/montreISTA/entity/Sellers.java
+++ src/main/java/com/codeblue/montreISTA/entity/Sellers.java
@@ -2,6 +2,7 @@ package com.codeblue.montreISTA.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
 
@@ -19,13 +20,13 @@ public class Sellers {
     @JoinColumn(name = "user_id")
     private Users userId;
 
-
+    @NotBlank(message = "name must not be blank")
     private String storeName;
 
-
+    @NotBlank(message = "store photo must not be blank")
     private String storePhoto;
 
-
+    @NotBlank(message = "store addre must not be blank")
     private String storeAddress;
 
 }
