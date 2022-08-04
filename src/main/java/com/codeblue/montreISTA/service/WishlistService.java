024e5f9a0099b9a4c5bd0ebb28eb7dba1b47743b
Rifhub <105769135+Rifhub@users.noreply.github.com>
2022-08-04 10:49:58 +0700

    wishlist, controller

--- src/main/java/com/codeblue/montreISTA/service/WishlistService.java
+++ src/main/java/com/codeblue/montreISTA/service/WishlistService.java
@@ -1,4 +1,49 @@
 package com.codeblue.montreISTA.service;
 
+
+import com.codeblue.montreISTA.entity.Seller;
+import com.codeblue.montreISTA.entity.Wishlist;
+import com.codeblue.montreISTA.repository.SellerRepository;
+import com.codeblue.montreISTA.repository.WishlistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class WishlistService {
+
+    @Autowired
+    WishlistRepository wishlistRepository;
+
+    public List<Wishlist> findAllWishlist() {
+        List<Wishlist> wishlists = wishlistRepository.findAll();
+        return wishlists;
+    }
+
+    public Optional<Wishlist> findWishlistById(Long id) {
+        return wishlistRepository.findById(id);
+    }
+
+    public Wishlist createWishlist(Wishlist wishlist) {
+        return wishlistRepository.save(wishlist);
+    }
+
+    public Wishlist updateWishlist(Wishlist updateWishlist) {
+        return wishlistRepository.save(updateWishlist);
+    }
+
+    public void deleteWishlist(Long id) {wishlistRepository.deleteById(id);
+    }
+
+    public List<Wishlist> findWishlisttByWishlistId(Long id) {
+        List<Wishlist> wishlist = wishlistRepository.findByWishlistId(id);
+        if(wishlist.isEmpty()){
+            return null;
+        } else {
+            return wishlist;
+        }
+    }
+
 }
