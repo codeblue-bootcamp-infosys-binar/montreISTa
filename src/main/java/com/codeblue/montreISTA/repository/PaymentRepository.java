bad6eb505404f0a90efe37a34293508da18d07b4
alynfmp <alynfmp@gmail.com>
2022-08-05 10:46:39 +0700

    order controller

--- src/main/java/com/codeblue/montreISTA/repository/PaymentRepository.java
+++ src/main/java/com/codeblue/montreISTA/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.codeblue.montreISTA.repository;
 
 
+import com.codeblue.montreISTA.entity.Order;
 import com.codeblue.montreISTA.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,8 +12,5 @@ import java.util.List;
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
 
-//    @Query("SELECT p FROM Payment p where p.Name LIKE %:name% ORDER BY p.Name ASC")
-//    public List<Payment> getPaymentByName(@Param("name") String name);
-//
-//    List<Payment> findByName(String name);
+    List<Payment> findByNameContaining(String keyword);
 }
