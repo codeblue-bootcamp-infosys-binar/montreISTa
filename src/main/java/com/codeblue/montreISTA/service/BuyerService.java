2e3465ffd035162ff38b386a365d1c834d12dc32
unknown <salahuddinade@gmail.com>
2022-08-05 11:09:31 +0700

    buyer

--- src/main/java/com/codeblue/montreISTA/service/BuyerService.java
+++ src/main/java/com/codeblue/montreISTA/service/BuyerService.java
@@ -1,45 +1,16 @@
-//package com.codeblue.montreISTA.service;
-//
-//import com.codeblue.montreISTA.entity.Buyer;
-//import com.codeblue.montreISTA.repository.BuyerRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//public class BuyerService {
-//    @Autowired
-//    BuyerRepository buyerRepository;
-//
-//    public List<Buyer> findAllBuyer() {
-//        List<Buyer> buyer = buyerRepository.findAll();
-//        return buyer;
-//    }
-//
-//    public Optional<Buyer> findBuyerById(Long id) {
-//        return buyerRepository.findById(id);
-//    }
-//
-//    public Buyer createBuyer(Buyer buyer) {
-//        return buyerRepository.save(buyer);
-//    }
-//
-//    public Buyer updateBuyer(Buyer updateBuyer) {
-//        return buyerRepository.save(updateBuyer);
-//    }
-//
-//    public void deleteBuyer(Long id) {
-//        buyerRepository.deleteById(id);
-//    }
-//
-//    public List<Buyer> findBuyertByBuyerId(Long id) {
-//        List<Buyer> buyer = buyerRepository.findByBuyerId(id);
-//        if(buyer.isEmpty()){
-//            return null;
-//        } else {
-//            return buyer;
-//        }
-//    }
-//}
+package com.codeblue.montreISTA.service;
+
+import com.codeblue.montreISTA.entity.Buyer;
+import com.codeblue.montreISTA.repository.BuyerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class BuyerService {
+
+}
