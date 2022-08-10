6d0ac0a426f5acfcfebc714954ef3627e6162a80
Rifhub <105769135+Rifhub@users.noreply.github.com>
2022-08-11 01:07:38 +0700

    logger wishlist

--- src/main/java/com/codeblue/montreISTA/controller/WishlistController.java
+++ src/main/java/com/codeblue/montreISTA/controller/WishlistController.java
@@ -2,7 +2,6 @@ package com.codeblue.montreISTA.controller;
 
 
 
-import com.codeblue.montreISTA.DTO.ProductResponseDTO;
 import com.codeblue.montreISTA.DTO.WishlistRequestDTO;
 import com.codeblue.montreISTA.DTO.WishlistResponseDTO;
 import com.codeblue.montreISTA.entity.Buyer;
@@ -15,21 +14,24 @@ import com.codeblue.montreISTA.response.ResponseHandler;
 import com.codeblue.montreISTA.service.WishlistService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
 @Tag(name="07. Wishlist")
 public class WishlistController {
+
+    private static final Logger logger = LoggerFactory.getLogger(WishlistController.class);
+
+    private static final String Line = "====================";
 
     private BuyerRepository buyerRepository;
 
@@ -43,8 +45,16 @@ public class WishlistController {
             List<Wishlist> wishlists = wishlistService.findAllWishlist();
             List <WishlistResponseDTO> wishlistResponseDTOS = wishlists.stream().map(DTOConverter::convertWishlist)
                     .collect(Collectors.toList());
-
-//
+            logger.info(Line + " Logger Start Find All Wishlist " + Line);
+            for (Wishlist dataresults:wishlists){
+                logger.info("================================");
+                logger.info("Wishlist id :"+dataresults.getWishlistId());
+                logger.info("Buyer ID :"+dataresults.getBuyer());
+                logger.info("Product ID :"+dataresults.getProduct());
+                logger.info("Quantity :"+dataresults.getQuantity());
+                logger.info("================================");
+            }
+            logger.info(Line + "Logger End Find All Wishlist" + Line);
 //            for (Wishlist wishlist : wishlists) {
 //                 Product product = wishlist.getProduct();
 ////                 products.add(product);
@@ -53,10 +63,12 @@ public class WishlistController {
 //                 WishlistResponseDTO wishlistDTO = wishlist.convertToResponse(productDTO);
 //                 wishlistResponseDTOS.add(wishlistDTO);
 //            }
-
-            return ResponseHandler.generateResponse("successfully retrieved wishlist", HttpStatus.OK, wishlistResponseDTOS);
+            return ResponseHandler.generateResponse("success get all wishlist", HttpStatus.OK, wishlistResponseDTOS);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error("------------------------------------");
+            logger.error(e.getMessage());
+            logger.error("------------------------------------");
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Table has no value!");
         }
     }
 
@@ -67,10 +79,15 @@ public class WishlistController {
             List<Wishlist> wishlist = wishlistService.findWishlisttByWishlistId(wishlistId);
             List <WishlistResponseDTO> wishlistResponseDTOS = wishlist.stream().map(DTOConverter::convertWishlist)
                     .collect(Collectors.toList());
-
-            return ResponseHandler.generateResponse("successfully retrieved wishlists", HttpStatus.OK, wishlistResponseDTOS);
+            logger.info(Line + "Logger Start Get by wishlist id " + Line);
+            logger.info(String.valueOf(wishlistResponseDTOS));
+            logger.info(Line + "Logger End Get by wishlist id " + Line);
+            return ResponseHandler.generateResponse("success get by wishlist id", HttpStatus.OK, wishlistResponseDTOS);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(),HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(),HttpStatus.NOT_FOUND, "Wishlist had no value!");
         }
     }
 
@@ -80,10 +97,15 @@ public class WishlistController {
             List<Wishlist> wishlists = wishlistService.findByBuyerUserName(keyword);
             List <WishlistResponseDTO> wishlistResponseDTOS = wishlists.stream().map(DTOConverter::convertWishlist)
                     .collect(Collectors.toList());
-
-            return ResponseHandler.generateResponse("successfully retrieved buyer username", HttpStatus.OK, wishlistResponseDTOS);
+            logger.info(Line + "Logger Start Get buyer username " + Line);
+            logger.info(String.valueOf(wishlistResponseDTOS));
+            logger.info(Line + "Logger End Get buyer username " + Line);
+            return ResponseHandler.generateResponse("success get by buyer username", HttpStatus.OK, wishlistResponseDTOS);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Wishlist had no value!");
         }
     }
 
@@ -93,10 +115,15 @@ public class WishlistController {
             List<Wishlist> wishlists = wishlistService.findByBuyerUserUsername(keyword);
             List <WishlistResponseDTO> wishlistResponseDTOS = wishlists.stream().map(DTOConverter::convertWishlist)
                     .collect(Collectors.toList());
-
-            return ResponseHandler.generateResponse("successfully retrieved buyer username", HttpStatus.OK, wishlistResponseDTOS);
+            logger.info(Line + "Logger Start Get buyer user username " + Line);
+            logger.info(String.valueOf(wishlistResponseDTOS));
+            logger.info(Line + "Logger End Get buyer user username " + Line);
+            return ResponseHandler.generateResponse("success get by buyer user username", HttpStatus.OK, wishlistResponseDTOS);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Wishlist had no value!");
         }
     }
 
@@ -106,10 +133,15 @@ public class WishlistController {
         try{
             Optional<Wishlist> wishlist = wishlistService.findWishlistById(id);
             WishlistResponseDTO wishlistResponseDTO = DTOConverter.convertWishlist(wishlist.get());
-
-            return ResponseHandler.generateResponse("successfully retrieved wishlist", HttpStatus.OK, wishlistResponseDTO);
+            logger.info(Line + "Logger Start Get By Id " + Line);
+            logger.info(String.valueOf(wishlistResponseDTO));
+            logger.info(Line + "Logger End Get By Id " + Line);
+            return ResponseHandler.generateResponse("success get by id", HttpStatus.OK, wishlistResponseDTO);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Wishlist had no value!");
         }
     }
 
@@ -122,10 +154,15 @@ public class WishlistController {
             Wishlist wishlist1 = wishlistRequestDTO.convertToEntity(buyer,product);
             Wishlist wishlist = wishlistService.createWishlist(wishlist1);
             WishlistResponseDTO wishlistResponseDTO = DTOConverter.convertWishlist(wishlist);
-
-            return ResponseHandler.generateResponse("successfully retrieved wishlist", HttpStatus.CREATED, wishlistResponseDTO);
+            logger.info(Line + "Logger Start Create " + Line);
+            logger.info(String.valueOf(wishlistResponseDTO));
+            logger.info(Line + "Logger End Create " + Line);
+            return ResponseHandler.generateResponse("successfully create wishlist", HttpStatus.CREATED, wishlistResponseDTO);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS,null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST,"Failed create wishlist!");
         }
     }
 
@@ -140,13 +177,17 @@ public class WishlistController {
             updateWishlist.setWishlistId(id);
             updateWishlist.setBuyer(buyer);
             updateWishlist.setProduct(product);
-
             wishlistService.updateWishlist(updateWishlist);
             WishlistResponseDTO wishlistResponseDTO = DTOConverter.convertWishlist(targetWishlist.get());
-
-            return ResponseHandler.generateResponse("successfully updated Wishlist", HttpStatus.CREATED,targetWishlist );
+            logger.info(Line + "Logger Start Update By Id " + Line);
+            logger.info(String.valueOf(targetWishlist));
+            logger.info(Line + "Logger End Update By Id " + Line);
+            return ResponseHandler.generateResponse("successfully updated Wishlist", HttpStatus.OK,targetWishlist );
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, "Failed update wishlist!");
         }
     }
 
@@ -155,9 +196,15 @@ public class WishlistController {
     public ResponseEntity<Object> deleteWishlist(@PathVariable("id") Long id){
         try{
             wishlistService.deleteWishlist(id);
-            return ResponseHandler.generateResponse("successfully deleted wishlist", HttpStatus.MULTI_STATUS, null);
+            logger.info(Line + "Logger Start Delete By Id " + Line);
+            logger.info("Delete Success");
+            logger.info(Line + "Logger End Delete By Id " + Line);
+            return ResponseHandler.generateResponse("successfully deleted wishlist", HttpStatus.OK, null);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, "Failed delete wishlist!");
         }
 
     }
