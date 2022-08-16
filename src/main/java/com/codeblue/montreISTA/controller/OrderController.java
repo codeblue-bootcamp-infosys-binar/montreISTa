a0709ba14f3c09e9c9eece46a1e4027d825068f2
Rifhub <105769135+Rifhub@users.noreply.github.com>
2022-08-16 10:27:13 +0700

    logger order

--- src/main/java/com/codeblue/montreISTA/controller/OrderController.java
+++ src/main/java/com/codeblue/montreISTA/controller/OrderController.java
@@ -5,16 +5,25 @@ import com.codeblue.montreISTA.response.ResponseHandler;
 import com.codeblue.montreISTA.service.OrderService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 @Tag(name="09. Order")
 public class OrderController {
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+    private static final String Line = "====================";
 
     private OrderService orderService;
 
@@ -25,10 +34,39 @@ public class OrderController {
     @GetMapping("/user/orders")
     public ResponseEntity<Object> getAllOrder(){
         try{
+            List<OrderResponseDTO> orders = orderService.findAllOrder();
             List<OrderResponseDTO> results = orderService.findAllOrder();
+            List<Map<String, Object>> maps = new ArrayList<>();
+            logger.info("==================== Logger Start Get All Order    ====================");
+            for(OrderResponseDTO orderData : orders){
+                Map<String, Object> order = new HashMap<>();
+                logger.info("-------------------------");
+                logger.info("Order ID                : " + orderData.getOrderId());
+                logger.info("Payment ID              : " + orderData.getPayment_id());
+                logger.info("Destionation Name       : " + orderData.getDestination_name());
+                logger.info("Destionation Address    : " + orderData.getDestination_address());
+                logger.info("Destionation Phone      : " + orderData.getDestination_phone());
+                logger.info("Shipping ID             : " + orderData.getShipping_id());
+                logger.info("Zip Code                : " + orderData.getZip_code());
+                logger.info("Total Price             : " + orderData.getTotal_price());
+                order.put("Order ID                : ", orderData.getOrderId());
+                order.put("Payment ID              : ", orderData.getPayment_id());
+                order.put("Destionation Name       : ", orderData.getDestination_name());
+                order.put("Destionation Address    : ", orderData.getDestination_address());
+                order.put("Destionation Phone      : ", orderData.getDestination_phone());
+                order.put("Shipping ID             : ", orderData.getShipping_id());
+                order.put("Zip Code                : ", orderData.getZip_code());
+                order.put("Total Price             : ", orderData.getTotal_price());
+                maps.add(order);
+            }
+            logger.info("==================== Logger End Get AlL Order   ====================");
+            logger.info(" ");
             return ResponseHandler.generateResponse("successfully retrieved orders", HttpStatus.OK, results);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Order had no value!");
         }
     }
 
@@ -41,9 +79,15 @@ public class OrderController {
     public ResponseEntity<Object> getOrderById(@PathVariable("id") Long id){
         try{
             OrderResponseDTO results = orderService.findOrderById(id);
+            logger.info(Line + "Logger Start Get By Id " + Line);
+            logger.info(String.valueOf(results));
+            logger.info(Line + "Logger End Get By Id " + Line);
             return ResponseHandler.generateResponse("successfully retrieved orders", HttpStatus.OK, results);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Order had no value!");
         }
     }
 
@@ -51,9 +95,15 @@ public class OrderController {
     public ResponseEntity<Object> getOrderByBuyerId(@PathVariable("id") Long id){
         try{
             OrderResponseCartDTO results = orderService.findByBuyerId(id);
+            logger.info(Line + "Logger Start Get Buyer Id " + Line);
+            logger.info(String.valueOf(results));
+            logger.info(Line + "Logger End Get Buyer Id " + Line);
             return ResponseHandler.generateResponse("successfully retrieved orders", HttpStatus.OK, results);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Order had no value!");
         }
     }
 
@@ -66,9 +116,15 @@ public class OrderController {
     public ResponseEntity<Object> findByProductName(@Param("keyword") String keyword){
         try{
             List<OrderResponseDTO> results = orderService.findByProductName(keyword);
+            logger.info(Line + "Logger Start Get Order By Productname  " + Line);
+            logger.info(String.valueOf(results));
+            logger.info(Line + "Logger End Get Order By Productname " + Line);
             return ResponseHandler.generateResponse("successfully retrieved orders", HttpStatus.OK, results);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Order no value!");
         }
     }
 
@@ -81,9 +137,15 @@ public class OrderController {
     public ResponseEntity<Object> findByBuyerUserName(@Param("keyword") String keyword){
         try{
             List<OrderResponseDTO> results = orderService.findByBuyerName(keyword);
+            logger.info(Line + "Logger Start Get Order By Username  " + Line);
+            logger.info(String.valueOf(results));
+            logger.info(Line + "Logger End Get Order By Username " + Line);
             return ResponseHandler.generateResponse("successfully retrieved orders", HttpStatus.OK, results);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Order no value!");
         }
     }
 
@@ -96,9 +158,15 @@ public class OrderController {
     public ResponseEntity<Object> findBySellerStoreName(@Param("keyword") String keyword){
         try{
             List<OrderResponseDTO> results = orderService.findByStoreName(keyword);
+            logger.info(Line + "Logger Start Get Order By Storename  " + Line);
+            logger.info(String.valueOf(results));
+            logger.info(Line + "Logger End Get Order By Storename " + Line);
             return ResponseHandler.generateResponse("successfully retrieved orders", HttpStatus.OK, results);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Order no value!");
         }
     }
 
@@ -110,9 +178,15 @@ public class OrderController {
     public ResponseEntity<Object> updateOrder(@RequestBody OrderRequestDTO orderRequestDTO, @PathVariable("id") Long id){
         try {
             OrderResponseDTO results = orderService.updateOrder(orderRequestDTO,id);
-            return ResponseHandler.generateResponse("successfully retrieved order", HttpStatus.CREATED, results);
+            logger.info(Line + "Logger Start Update By Id " + Line);
+            logger.info(String.valueOf(results));
+            logger.info(Line + "Logger End Update By Id " + Line);
+            return ResponseHandler.generateResponse("successfully retrieved order", HttpStatus.OK, results);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS,null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST,"Failed update order!");
         }
     }
 
@@ -125,9 +199,15 @@ public class OrderController {
     public ResponseEntity<Object> deleteOrder(@PathVariable("id") Long id){
         try{
             orderService.deleteOrder(id);
-            return ResponseHandler.generateResponse("successfully deleted Order", HttpStatus.MULTI_STATUS, null);
+            logger.info(Line + "Logger Start Delete By Id " + Line);
+            logger.info("Delete Success");
+            logger.info(Line + "Logger End Delete By Id " + Line);
+            return ResponseHandler.generateResponse("successfully deleted Order", HttpStatus.OK, null);
         } catch (Exception e){
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, "Failed Delete Order!");
         }
 
     }
