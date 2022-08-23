6b7841bc0a6c34efa14eb0014c6cc13fe3fb09cc
Rifhub <105769135+Rifhub@users.noreply.github.com>
2022-08-23 10:32:48 +0700

    clean code payment logger

--- src/main/java/com/codeblue/montreISTA/controller/PaymentController.java
+++ src/main/java/com/codeblue/montreISTA/controller/PaymentController.java
@@ -36,19 +36,8 @@ public class PaymentController {
      *
      */
     @PostMapping("/dashboard/payment/create")
-    public ResponseEntity<Object> addPayment(@RequestBody PaymentRequestDTO paymentRequestDTO) {
-        try {
-            PaymentResponseDTO payment = paymentService.addPayment(paymentRequestDTO);
-            logger.info(Line + "Logger Start Create " + Line);
-            logger.info(String.valueOf(payment));
-            logger.info(Line + "Logger End Create " + Line);
-            return ResponseHandler.generateResponse("successfully retrieved payment", HttpStatus.CREATED, payment);
-        } catch (Exception e) {
-            logger.error(Line + " Logger Start Error " + Line);
-            logger.error(e.getMessage());
-            logger.error(Line + " Logger End Error " + Line);
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, "Failed create payment!");
-        }
+    public ResponseEntity<Object> addPayment(@RequestBody PaymentRequestDTO paymentRequestDTO) throws Exception {
+          return paymentService.addPayment(paymentRequestDTO);
     }
 
     /**
@@ -56,26 +45,8 @@ public class PaymentController {
      * @return
      */
     @GetMapping("/payment")
-    public ResponseEntity<Object> getAllPayment(){
-        try{
-            List<PaymentResponseDTO> payments = paymentService.findAllPayment();
-                logger.info("==================== Logger Start Get All Payments     ====================");
-            for(PaymentResponseDTO paymentData : payments){
-                logger.info("-------------------------");
-                logger.info("Payment ID    : " + paymentData.getPayment_id());
-                logger.info("Name          : "      + paymentData.getName());
-                logger.info("Payment Code  : " + paymentData.getPayment_Code());
-            }
-            logger.info("==================== Logger End Get All Payments     ====================");
-            logger.info(" ");
-            return ResponseHandler.generateResponse("success get all payment", HttpStatus.OK, payments);
-        } catch (Exception e){
-            logger.info("==================== Logger Start Get All Payments     ====================");
-            logger.error(String.valueOf(ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND,"Table has no value")));
-            logger.info("==================== Logger End Get All Payments     ====================");
-            logger.info(" ");
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, "Table had no value!");
-        }
+    public ResponseEntity<Object> getAllPayment() throws Exception {
+            return paymentService.findAllPayment();
     }
 
     /**
@@ -84,19 +55,8 @@ public class PaymentController {
      * @return
      */
     @GetMapping("/payment/paymentName")
-    public ResponseEntity<Object> findByPaymentName(@Param("keyword") String keyword){
-        try{
-            List<PaymentResponseDTO> payments = paymentService.findByPaymentName(keyword);
-            logger.info(Line + "Logger Start Get By Id " + Line);
-            logger.info(String.valueOf(payments));
-            logger.info(Line + "Logger End Get By Id " + Line);
-            return ResponseHandler.generateResponse("successfully retrieved payment", HttpStatus.OK, payments);
-        } catch (Exception e){
-            logger.error(Line + " Logger Start Error " + Line);
-            logger.error(e.getMessage());
-            logger.error(Line + " Logger End Error " + Line);
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Payment had no value!");
-        }
+    public ResponseEntity<Object> findByPaymentName(@Param("keyword") String keyword) throws Exception {
+            return paymentService.findByPaymentName(keyword);
     }
 
     /**
@@ -104,19 +64,9 @@ public class PaymentController {
      * @return
      */
     @GetMapping("/payment/{id}")
-    public ResponseEntity<Object> getPaymentById(@PathVariable("id") Long id) {
-        try{
-            PaymentResponseDTO payment = paymentService.findPaymentById(id);
-            logger.info(Line + "Logger Start Get By Id " + Line);
-            logger.info(String.valueOf(payment));
-            logger.info(Line + "Logger End Get By Id " + Line);
-            return ResponseHandler.generateResponse("success get by id", HttpStatus.OK, payment);
-        } catch (Exception e){
-            logger.error(Line + " Logger Start Error " + Line);
-            logger.error(e.getMessage());
-            logger.error(Line + " Logger End Error " + Line);
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Payment had no value!");
-        }
+    public ResponseEntity<Object> getPaymentById(@PathVariable("id") Long id) throws Exception {
+            return paymentService.findPaymentById(id);
+
     }
 
     /**
@@ -126,19 +76,8 @@ public class PaymentController {
      * @return
      */
     @PutMapping("/dashboard/payment/update/{id}")
-    public ResponseEntity<Object> updatePayment(@RequestBody PaymentRequestDTO PaymentResponseDTO, @PathVariable("id") Long id) {
-        try {
-            PaymentResponseDTO results = paymentService.updatePayment(PaymentResponseDTO,id);
-            logger.info(Line + "Logger Start Update By Id " + Line);
-            logger.info(String.valueOf(results));
-            logger.info(Line + "Logger End Update By Id " + Line);
-            return ResponseHandler.generateResponse("successfully updated payment", HttpStatus.OK, results);
-        } catch (Exception e) {
-            logger.error(Line + " Logger Start Error " + Line);
-            logger.error(e.getMessage());
-            logger.error(Line + " Logger End Error " + Line);
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, "Failed update payment!");
-        }
+    public ResponseEntity<Object> updatePayment(@RequestBody PaymentRequestDTO PaymentResponseDTO, @PathVariable("id") Long id) throws Exception {
+            return paymentService.updatePayment(PaymentResponseDTO,id);
     }
 
 
@@ -148,20 +87,8 @@ public class PaymentController {
      * @return
      */
     @DeleteMapping("/dashboard/payment/delete/{id}")
-    public ResponseEntity<Object> deletePayment(@PathVariable("id") Long id) {
-    try{
-        paymentService.deletePayment(id);
-        logger.info(Line + "Logger Start Delete By Id " + Line);
-        logger.info("Delete Success");
-        logger.info(Line + "Logger End Delete By Id " + Line);
-        return ResponseHandler.generateResponse("successfully deleted payment", HttpStatus.OK, "deleted");
-    } catch(Exception e){
-        logger.error(Line + " Logger Start Error " + Line);
-        logger.error(e.getMessage());
-        logger.error(Line + " Logger End Error " + Line);
-        return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, "Failed data payment!");
-    }
-
+    public ResponseEntity<Object> deletePayment(@PathVariable("id") Long id) throws Exception {
+       return paymentService.deletePayment(id);
     }
 
 }
--- src/main/java/com/codeblue/montreISTA/service/implement/PaymentServiceImpl.java
+++ src/main/java/com/codeblue/montreISTA/service/implement/PaymentServiceImpl.java
@@ -2,10 +2,16 @@ package com.codeblue.montreISTA.service.implement;
 
 import com.codeblue.montreISTA.DTO.PaymentRequestDTO;
 import com.codeblue.montreISTA.DTO.PaymentResponseDTO;
+import com.codeblue.montreISTA.controller.PaymentController;
 import com.codeblue.montreISTA.entity.Payment;
 import com.codeblue.montreISTA.repository.PaymentRepository;
+import com.codeblue.montreISTA.response.ResponseHandler;
 import com.codeblue.montreISTA.service.PaymentService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,63 +21,135 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(PaymentController.class);
+
+    private static final String Line = "====================";
+
     private final PaymentRepository paymentRepository;
 
     @Override
-    public List<PaymentResponseDTO> findAllPayment() throws Exception {
+    public ResponseEntity<Object> findAllPayment() throws Exception {
+        try{
         List<PaymentResponseDTO> payments = paymentRepository.findAll().stream()
                 .map(Payment::convertToResponse)
                 .collect(Collectors.toList());
-        if(payments.isEmpty()){
+        if (payments.isEmpty()) {
             throw new Exception("Payment not found");
         }
-        return payments;
+        logger.info("==================== Logger Start Get All Payments     ====================");
+        for (PaymentResponseDTO paymentData : payments) {
+            logger.info("-------------------------");
+            logger.info("Payment ID    : " + paymentData.getPayment_id());
+            logger.info("Name          : " + paymentData.getName());
+            logger.info("Payment Code  : " + paymentData.getPayment_Code());
+        }
+        logger.info("==================== Logger End Get All Payments     ====================");
+        logger.info(" ");
+        return ResponseHandler.generateResponse("success get all payment", HttpStatus.OK, payments);
+    } catch(Exception e){
+        logger.info("==================== Logger Start Get All Payments     ====================");
+        logger.error(String.valueOf(ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Table has no value")));
+        logger.info("==================== Logger End Get All Payments     ====================");
+        logger.info(" ");
+        return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, "Table had no value!");
+    }
+
+}
+
+    @Override
+    public ResponseEntity<Object> findPaymentById(Long id) throws Exception {
+        try{
+        Payment payment = paymentRepository.findById(id).orElseThrow(() -> new Exception("Payment not found"));
+            logger.info(Line + "Logger Start Get By Id " + Line);
+            logger.info(String.valueOf(payment));
+            logger.info(Line + "Logger End Get By Id " + Line);
+            return ResponseHandler.generateResponse("success get by id", HttpStatus.OK, payment);
+        } catch (Exception e){
+            logger.error(Line + " Logger Start Error " + Line);
+            logger.error(e.getMessage());
+            logger.error(Line + " Logger End Error " + Line);
+            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Payment had no value!");
+        }
     }
 
     @Override
-    public PaymentResponseDTO findPaymentById(Long id) throws Exception{
-        Payment payment = paymentRepository.findById(id).orElseThrow(()->new Exception("Payment not found"));
-        return payment.convertToResponse();
-    }
-
-    @Override
-    public List<PaymentResponseDTO> findByPaymentName(String keyword)throws Exception {
+    public ResponseEntity<Object> findByPaymentName(String keyword) throws Exception {
+        try{
         List<Payment> payments = paymentRepository.findByNameIgnoreCaseContaining(keyword);
-        if(payments.isEmpty()){
+        if (payments.isEmpty()) {
             throw new Exception("Payment not found");
         }
-        return payments.stream()
-                .map(Payment::convertToResponse)
-                .collect(Collectors.toList());
+        logger.info(Line + "Logger Start Get By Id " + Line);
+        logger.info(String.valueOf(payments));
+        logger.info(Line + "Logger End Get By Id " + Line);
+        return ResponseHandler.generateResponse("successfully retrieved payment", HttpStatus.OK, payments);
+    } catch (Exception e){
+        logger.error(Line + " Logger Start Error " + Line);
+        logger.error(e.getMessage());
+        logger.error(Line + " Logger End Error " + Line);
+        return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Payment had no value!");
+    }
     }
 
     @Override
-    public PaymentResponseDTO addPayment(PaymentRequestDTO paymentRequestDTO) throws Exception {
+    public ResponseEntity<Object> addPayment(PaymentRequestDTO paymentRequestDTO) throws Exception {
+        try{
         Payment payment = paymentRequestDTO.convertToEntity();
         List<Payment> payments = paymentRepository.findByNameIgnoreCaseContaining(paymentRequestDTO.getName());
-        if(!payments.isEmpty()){
+        if (!payments.isEmpty()) {
             throw new Exception("Can't update with same name");
         }
-        return paymentRepository.save(payment).convertToResponse();
+        logger.info(Line + "Logger Start Create " + Line);
+        logger.info(String.valueOf(payment));
+        logger.info(Line + "Logger End Create " + Line);
+        return ResponseHandler.generateResponse("successfully retrieved payment", HttpStatus.CREATED, payment);
+    } catch (Exception e) {
+        logger.error(Line + " Logger Start Error " + Line);
+        logger.error(e.getMessage());
+        logger.error(Line + " Logger End Error " + Line);
+        return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, "Failed create payment!");
+    }
     }
 
     @Override
-    public PaymentResponseDTO updatePayment(PaymentRequestDTO paymentRequestDTO, Long id) throws Exception {
-        Payment payment = paymentRepository.findById(id).orElseThrow(()->new Exception("Payment not found"));
+    public ResponseEntity<Object> updatePayment(PaymentRequestDTO paymentRequestDTO, Long id) throws Exception {
+        try{
+        Payment payment = paymentRepository.findById(id).orElseThrow(() -> new Exception("Payment not found"));
         List<Payment> payments = paymentRepository.findByNameIgnoreCaseContaining(paymentRequestDTO.getName());
-        if(!payments.isEmpty()){
+        if (!payments.isEmpty()) {
             throw new Exception("Can't update with same name");
         }
         payment.setPaymentId(id);
         payment.setName(paymentRequestDTO.getName());
         payment.setPaymentCode(paymentRequestDTO.getPayment_code());
-        return paymentRepository.save(payment).convertToResponse();
+        logger.info(Line + "Logger Start Update By Id " + Line);
+        logger.info(String.valueOf(payment));
+        logger.info(Line + "Logger End Update By Id " + Line);
+        return ResponseHandler.generateResponse("successfully updated payment", HttpStatus.OK, payment);
+    } catch (Exception e) {
+        logger.error(Line + " Logger Start Error " + Line);
+        logger.error(e.getMessage());
+        logger.error(Line + " Logger End Error " + Line);
+        return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, "Failed update payment!");
+    }
     }
 
     @Override
-    public void deletePayment(Long paymentId) throws Exception {
-        paymentRepository.findById(paymentId).orElseThrow(()->new Exception("Payment not found"));
+    public ResponseEntity<Object> deletePayment(Long paymentId) throws Exception {
+        try{
+        paymentRepository.findById(paymentId).orElseThrow(() -> new Exception("Payment not found"));
         paymentRepository.deleteById(paymentId);
+        logger.info(Line + "Logger Start Delete By Id " + Line);
+        logger.info("Delete Success");
+        logger.info(Line + "Logger End Delete By Id " + Line);
+        return ResponseHandler.generateResponse("successfully deleted payment", HttpStatus.OK, "deleted");
+    } catch(Exception e){
+        logger.error(Line + " Logger Start Error " + Line);
+        logger.error(e.getMessage());
+        logger.error(Line + " Logger End Error " + Line);
+        return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.BAD_REQUEST, "Failed data payment!");
+    }
     }
 
 }
