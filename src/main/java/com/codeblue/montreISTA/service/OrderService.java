2bfbaa773393ee2a45e9645b92057d7c0eb0fd5d
unknown <salahuddinade@gmail.com>
2022-08-24 12:12:05 +0700

    photo, order

--- src/main/java/com/codeblue/montreISTA/service/OrderService.java
+++ src/main/java/com/codeblue/montreISTA/service/OrderService.java
@@ -3,22 +3,23 @@ package com.codeblue.montreISTA.service;
 import com.codeblue.montreISTA.DTO.OrderRequestDTO;
 import com.codeblue.montreISTA.DTO.OrderResponseCartDTO;
 import com.codeblue.montreISTA.DTO.OrderResponseDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 
 import java.util.List;
 
 public interface OrderService {
 
-    List<OrderResponseDTO> findAllOrder();
-    OrderResponseDTO findOrderById(Long id) throws Exception;
+    ResponseEntity<Object> findAllOrder();
+    ResponseEntity<Object> findOrderById(Long id) throws Exception;
 
-    List<OrderResponseDTO> findByProductName(String keyword) throws Exception;
+    ResponseEntity<Object> findByProductName(String keyword) throws Exception;
     List<OrderResponseDTO> findByBuyerId(Long id) throws Exception;
-    List<OrderResponseDTO> findByStoreName(String keyword) throws Exception;
-    OrderResponseCartDTO findByBuyer(String keyword)throws Exception;
-    OrderResponseDTO updateOrder(OrderRequestDTO orderRequestDTO, String keyword) throws Exception;
+    ResponseEntity<Object> findByStoreName(String keyword) throws Exception;
+    ResponseEntity<Object> findByBuyer(String keyword)throws Exception;
+    ResponseEntity<Object> updateOrder(OrderRequestDTO orderRequestDTO, String keyword) throws Exception;
 
-    void deleteOrder(Authentication authentication) throws Exception;
+    ResponseEntity<Object> deleteOrder(Authentication authentication) throws Exception;
 
-    OrderResponseCartDTO payOrder(String keyword)throws Exception;
+    ResponseEntity<Object> payOrder(String keyword)throws Exception;
 }
